1bf0a36167e29501fc9092f8fa754411d8b278ca
Paul Kompfner <paul@daily.co>
2025-01-08 11:02:16 -0500

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -15,18 +15,14 @@ let package = Package(
     ],
     dependencies: [
         // Local dependency
-//        .package(path: "../rtvi-client-ios"),
-         .package(url: "https://github.com/pipecat-ai/pipecat-client-ios.git", from: "0.3.0"),
-        // TODO: we can remove this once we're done with the prototyping. Useful to have it here to refer to types
-        .package(url: "https://github.com/daily-co/daily-client-ios.git", from: "0.23.0")
+//        .package(path: "../pipecat-client-ios"),
+         .package(url: "https://github.com/pipecat-ai/pipecat-client-ios.git", from: "0.3.1"),
     ],
     targets: [
         .target(
             name: "RTVIClientIOSGeminiLiveWebSocket",
             dependencies: [
-                .product(name: "PipecatClientIOS", package: "pipecat-client-ios"),
-                // TODO: we can remove this once we're done with the prototyping. Useful to have it here to refer to types
-                .product(name: "Daily", package: "daily-client-ios")
+                .product(name: "PipecatClientIOS", package: "pipecat-client-ios")
             ]),
         .testTarget(
             name: "RTVIClientIOSGeminiLiveWebSocketTests",
